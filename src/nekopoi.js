a534d942a95c9a170151241f8b0cfe9495031cd0
Dark2k15 <80169758+Dark2k15@users.noreply.github.com>
2021-03-07 20:07:37 -0300

    Delete src directory

--- src/nekopoi.js
+++
@@ -1,9 +0,0 @@
-const nekopoi = () => { 
-	return `       
-Acesso ruim / assistindo Nekopoi sem baixar VPN? Clique no link abaixo, role para baixo e digite concordar e conectar.
-
-https://www.hidemyass-freeproxy.com/proxy/id-id/aHR0cHM6Ly9uZWtvcG9pLmNhcmUv
-
-*Selamat Menonton*`
-}
-exports.nekopoi = nekopoi
